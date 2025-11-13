a216ca8287715a3dd5035a51ddb21036deeb8276
Raul <raul.marks@outlook.com>
2025-11-13 12:23:04 +0200

    Added endpoint to list users

---
+++ WebShop.Users/UserEndpoints/ListUsersEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using WebShop.Users.Data;
+
+namespace WebShop.Users.UserEndpoints;
+
+internal record ListUsersResponse(IEnumerable<UserDto> Users);
+internal record UserDto(string? UserName, string? Email, string? FullName);
+
+internal class ListUsersEndpoint(UsersDbContext dbContext) : EndpointWithoutRequest<ListUsersResponse>
+{
+    private readonly UsersDbContext _dbContext = dbContext;
+
+    public override void Configure()
+    {
+        Get("/users");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var users = await _dbContext.ApplicationUsers
+            .AsNoTracking()
+            .Select(au => new UserDto(au.UserName, au.Email, au.FullName))
+            .ToListAsync();
+
+        await Send.OkAsync(new ListUsersResponse(users));
+    }
+}
